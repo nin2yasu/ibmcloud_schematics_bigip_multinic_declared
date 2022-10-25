00cd52edd12ba2b668f28e3cf62e0ccd65e98edd
syasuda <47959665+nin2yasu@users.noreply.github.com>
2022-10-25 13:34:36 +0900

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -126,13 +126,6 @@ data "template_file" "user_data" {
   }
 }
 
-# lookup reservedip by name
-data "ibm_is_subnet_reserved_ip" "management_reserved_ip" {
-  subnet      = data.ibm_is_subnet.f5_management_subnet.id
-  reserved_ip = var.management_reserved_ip
-}
-
-
 # create compute instance
 resource "ibm_is_instance" "f5_ve_instance" {
   name           = var.instance_name
@@ -144,7 +137,7 @@ resource "ibm_is_instance" "f5_ve_instance" {
     subnet          = data.ibm_is_subnet.f5_management_subnet.id
     security_groups = [ibm_is_security_group.f5_open_sg.id]
     primary_ip {
-      address = data.ibm_is_subnet_reserved_ip.management_reserved_ip.id
+      reserved_ip = var.management_reserved_ip
       auto_delete = false
     } 
   }
